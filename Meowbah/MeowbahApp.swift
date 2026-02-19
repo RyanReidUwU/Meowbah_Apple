7b168853c2d052d7c34f39d371fe7812cbc9b614
Uwasa of the Lost Nekomata <103368078+RyanReidUwU@users.noreply.github.com>
2026-02-19 22:34:05 +0000

    Initial Commit

---
+++ Meowbah/MeowbahApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowbahApp.swift
+//  Meowbah
+//
+//  Created by Ryan Reid on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MeowbahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
